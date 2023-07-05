2f2b6b97c4edaf9ef2a67d44dbd783d79c8c3dc4
Egor Prasolov <egor65749@gmail.com>
2023-07-05 14:54:21 -0400

    DFS BFS abstract

---
+++ searching.js
@@ -0,0 +1,23 @@
+//Linear Search
+//A basic search algorithm that checks every element until the right one is found
+//Worst case is that the program will have to go over every element
+
+//Binary Search
+//If the input list is sorted Binary search is the better algorithm to use O(log(n))
+
+//Breadth first search
+//Start from the first node, then move from left to right in the second level and so on.
+//Uses additional memory because this method needs to track every child node
+
+//Depth first search
+//Goes down all the way to the leaf node, then checks if the parent of left leaf node 
+//has a right child if not then the program returns to the root node
+
+//BFS vs DFS
+//BFS and DFS have an O(n) time complexity
+//BFS has the shortest path and closer nodes, but uses more memory
+//DFS use less memory, useful to check if path exists, can get slow
+
+
+
+
