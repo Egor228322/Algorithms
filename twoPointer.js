479bd6d61721b09a96d9a3946c5360ee3d34b66d
Egor Prasolov <egor65749@gmail.com>
2023-07-31 18:44:38 -0400

    Two Pointers

---
+++ twoPointer.js
@@ -0,0 +1,6 @@
+//Two Pointer technique
+//Strings and arrays benefit largely from this techique
+//This technique is helpful when we have to analyze 
+//each element of the collection compared to its other elements
+//As a result we can process two elements per loop instead of one
+
