6cd4c3aa847fa3fb05ba596dbb5e67551e8a154b
Egor Prasolov <egor65749@gmail.com>
2023-07-31 21:08:49 -0400

    Sliding Window dynamic

--- SlidingWindow.js
+++ SlidingWindow.js
@@ -27,4 +27,38 @@ const sumSubArr = function(arr, k) {
 
 } 
 
-console.log(sumSubArr(arr, k));
+/* console.log(sumSubArr(arr, k)); */
+
+//Dynamic Sliding Window
+//This sliding window technique involves starting of with a large window and then shrinking it
+
+//Find the shortest subarray which has a sum greater than the target
+
+const sample = [1,2,3,4,5,6,7,8,11];
+const target = 11;
+
+const smallestSubArr = function(arr, t) {
+
+    let minLength = Infinity;
+    let sum = 0;
+    let start = 0;
+    let end  = 0;
+
+    while (end !== arr.length) {
+
+        sum += arr[end];
+        end++;
+
+        while (start < end && sum >= target) {
+            sum -= arr[start];
+            start++;
+
+            minLength = Math.min(minLength, end - start + 1);
+        }
+    }
+
+    return minLength;
+
+}
+
+console.log(smallestSubArr(sample, target));
