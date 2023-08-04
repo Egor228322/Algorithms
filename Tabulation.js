122518aba84afd8b87a994ebb459a7bf67c2d1f7
Egor Prasolov <egor65749@gmail.com>
2023-08-03 20:22:30 -0400

    Tabulation gridtravel

--- Tabulation.js
+++ Tabulation.js
@@ -24,3 +24,38 @@ console.log(fib(8));
 console.log(fib(50)); */
 
 //Grid Traveler
+
+
+/* const gridTravel = function(r, c) {
+
+    const array = new Array(r + 1).fill(0).map(el => new Array(c + 1).fill(0));
+    array[1][1] = 1;
+
+    for (let i = 0; i <= r; i++) {
+        for (let j = 0; j <= c; j++) {
+            if (j + 1 <= c) array[i][j+1] += array[i][j];
+            if (i + 1 <= r) array[i + 1][j] += array[i][j];
+        }
+    }
+
+    return array[r][c];
+
+}   
+
+console.log(gridTravel(4,4)); */
+
+const gridTravel = function(r, c) {
+    const array = new Array(r + 1).fill(0).map(() => new Array(c + 1).fill(0));
+    array[1][1] = 1;
+
+    for (let i = 0; i <= r; i++) {
+        for (let j = 0; j <= c; j++) {
+            if (j + 1 <= c) array[i][j+1] += array[i][j];
+            if (i + 1 <= r) array[i + 1][j] += array[i][j];
+        }
+    }
+
+    return array[r][c];
+}
+
+console.log(gridTravel(18, 18));
